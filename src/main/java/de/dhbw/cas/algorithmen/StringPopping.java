29ca6bda87ccda307947f5fdb8ff797310827ef1
Benedikt <boelz.boelz@web.de>
2020-02-24 01:19:52 +0100

    Update StringPopping.java

--- src/main/java/de/dhbw/cas/algorithmen/StringPopping.java
+++ src/main/java/de/dhbw/cas/algorithmen/StringPopping.java
@@ -4,7 +4,7 @@ import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
-public class StringPopping {
+class StringPopping{
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
@@ -24,6 +24,7 @@ public class StringPopping {
 		}
 		
 		s.close();
+		System.exit(0);
 	}
 	
 	public static List<Integer> popStrings(int listLength, List<String> asList) {
@@ -39,8 +40,9 @@ public class StringPopping {
 	}
 
 	private static int popSingleString(String str) {
-		
-		
+		if(str == null || "".contentEquals(str)) {
+			return 1;
+		}
 		
 		String workString = ""+str;
 		
@@ -50,47 +52,57 @@ public class StringPopping {
 				return 0;
 			}
 			
-			boolean didPop = false;
 			char prevChar=workString.charAt(0);
 			int prevCharIndex =0;
 			boolean isMulti = false;
+			boolean didPop = false;
 			
 			for(i=1;i<workString.length(); i++) {
 				char currentChar = workString.charAt(i);
 				if(prevChar == currentChar) {
 					isMulti = true;
 				}
-				else {
-					// kann ich poppen?
-					
-					if(isMulti) {
-						boolean canPop = false;
-						//wenn nur ein letztes Einzelnes kommt koennen wir poppen. 
-						// da dann ja die Abbruchbedingung oben gilt.
-						//ist aber nicht effizient
-						int beginIndexOtherChar = -1;
-						for(int j=i+1; j<workString.length(); j++) {
-							if(workString.charAt(j) != currentChar && beginIndexOtherChar != -1 && j-beginIndexOtherChar == 1) {
-								canPop = true;
-							} else {
-								beginIndexOtherChar = j;								
+				else if(isMulti){
+					boolean canPop= false;
+					//check if we can pop
+					if(i==workString.length()-1) {
+						canPop=true;
+					}
+//					else if(i+1 < workString.length() && workString.charAt(i+1) == currentChar) {
+//						
+//					}
+					else {
+						//gibt an, ob das 
+						boolean isNextOfPopCharMulti = false;
+						// gibt an ob mind. 1 weiteres vom currentChar abweichendes Zeichen gefunden wurde, dann pop
+						boolean foundNext = false; 
+						
+						int indexOfFirstNext = -1;
+						for(int j= i+1; j< workString.length(); j++) {
+							if(workString.charAt(j) != currentChar) {
+								foundNext = true;
+								if(indexOfFirstNext == -1) {
+									indexOfFirstNext = j;
+								}
+								else if(j-indexOfFirstNext == 1) {
+									isNextOfPopCharMulti = true;
+									break; // frueher abbrechen, die ganze schleife muss nicht weiter durchlaufen werden.
+								}
 							}
 						}
-						if(!canPop) {
-							canPop = beginIndexOtherChar == workString.length() -1 || i==workString.length() -1
-							|| (i+1 < workString.length() && workString.charAt(i+1) == currentChar);
-						}
-						
-						if(canPop) {
-							workString = 
-									workString.substring(0, prevCharIndex) 
-									+ workString.substring(i, workString.length());
-							didPop = true;							
-						}
-						if(didPop) {
-							break;
-						}
+						canPop = isNextOfPopCharMulti || !foundNext;
 					}
+					if(canPop) {
+						//pop
+						workString = 
+								workString.substring(0, prevCharIndex) 
+								+ workString.substring(i, workString.length());
+						didPop = true;
+						break;
+					}
+					
+					
+				}else {
 					prevChar = currentChar;
 					prevCharIndex = i;
 				}
